c0c0dceaba4df54a76d669e7c197f966425a4ece
Julian de Ruiter <julianderuiter@gmail.com>
2016-08-28 13:09:50 +0200

    Bump version: 0.0.1 → 0.1.0.dev0

--- geneviz/__init__.py
+++ geneviz/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'Julian de Ruiter'
 __email__ = 'julianderuiter@gmail.com'
-__version__ = '0.0.1'
+__version__ = '0.1.0.dev0'
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='geneviz',
-    version='0.0.1',
+    version='0.1.0.dev0',
     description="Python library for creating visualizations of genomic data.",
     long_description=readme + '\n\n' + history,
     author="Julian de Ruiter",
